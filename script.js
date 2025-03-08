53a795302dea23be81e277e7f1c0c2c8f22484f1
EdnaEustaquio <eustaquiosihsa@gmail.com>
2025-03-07 18:38:47 -0600

    cambios en script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let lista = [];
+let lista2 = [];
+
+function crearLista (){
+
+    let numero = document.getElementById("numeroIngresado").value;
+    if (numero == "")
+    { alert("Por favor, inserte un numero.")}
+    else{
+        numero = parseInt(numero);
+        let numero2 = numero;
+        lista.push(numero);
+        lista2.push(numero);
+    }}
+
+function calcular (){
+    //duplic
+    lista=lista.concat(lista2);
+    let mayor;
+    let medio;
+    let menor;
+    for (let i =0; i <= lista[2]; i++){
+        if (lista[i]>lista[i+1]){
+            if(lista[i]>lista[i+2]){
+                mayor = lista[i];
+            }
+            else { medio = lista[i];}
+        }
+        else{
+            if(lista[i]> lista[i+2]){
+                medio = lista[i];
+            }
+            else {
+                menor = lista[i];
+            }
+        
+        }
+
+    }
+}
